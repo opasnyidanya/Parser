1e778f2ea647dcb8d8c50ac24043459dfb2c6354
opasnyidanya <opasnyidanya@mail.ru>
2017-05-17 21:57:26 +0300

    Update proj_with_menu.cpp

--- trunk/proj_with_menu.cpp
+++ trunk/proj_with_menu.cpp
@@ -4,46 +4,55 @@
 #include <string>
 #include <cstring>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
 struct user { //хранение данных файла в структуре
-    string name;
-    string id;
+    string tag;
+    string request;
 };
 
-void check (int &Answer, string &search, user &data) {
+void check (int &Answer, user &search, vector<user> &data) {
     switch(Answer) {
-    case 1: { //если задали поиск по имени
-        if(search == data.name) {// если запрос == имени - выводим
-            cout << "person name=" << data.name;
-            cout << " ID=" << data.id << endl ;
+    case 1: { //если задали поиск по имени/ID
+        for(int i = 0; i < data.size(); i++) {
+            if(data[i].tag == search.tag && data[i].request == search.request) {
+                for(int j = 0; j < data.size(); j++) {
+                    cout << data[j].request << ends;
+                }
+                cout << endl;
+                break;
+            }
         }
         break;
     }
-    case 2: { // если по id
-        if(search == data.id) {// если запрос == id - выводим
-            cout << "person name=" << data.name;
-            cout << " ID=" << data.id << endl ;
+    case 2: { //если всех вывести
+        for(int j = 0; j < data.size(); j++) {
+            cout << data[j].request << ends;
         }
+        cout << endl;
         break;
     }
-    case 3: { //если всех вывести
-        cout << "person name=" << data.name;
-        cout << " ID=" << data.id << endl ;
-        break;
+    case 4: {//если записывать в файл
+        ofstream file("data.txt", ios::app);
+        for(int j = 0; j < data.size(); j++) {
+            file << data[j].request << ends;
+        }
+        file << endl;
+        file.close();
     }
     }
 }
 
-void parser_xml (int &Answer, string &search) {
-    user write; //запись всех пользователей
+void parser_xml (int &Answer, user &search) {
+    vector<user> write; //запись всех пользователей
     string buf; // буффер для строки файла
-    int choice = 0; //1 - запись имени, 2 - ID
     int pos = 0; //позиция символов в строке
     vector<string> tags; //вектор тегов
 
     ifstream base("base.xml"); //открываем файл
+
     while(!base.eof()) { //до конца файла читаем
         getline(base, buf); //пишем строку из файла в буффер
 
@@ -54,7 +63,6 @@ void parser_xml (int &Answer, string &search) {
                 string buf_for_next(buf, pos, buf.size()); // создаем строку с остатком
                 buf = buf_for_next; //присваиваем изначальной строке остаток
 
-
                 if(buf2[1] != '/') { // если тег открытия - записываем в вектор
                     tags.push_back(buf2);
                 }
@@ -64,31 +72,26 @@ void parser_xml (int &Answer, string &search) {
                     if(tags[tags.size() - 1] == buf2) //если последний открытый тег равен закрывающему - удаляем из вектора
                         tags.erase(tags.end());
                     else { //не закрыли - ошибка и выход из программы
-                        cout << endl << "Error! tag " << tags[tags.size() - 1] << " didn't close!" << endl;
+                        cout << endl << "Error! Tag " << tags[tags.size() - 1] << " is wrong!" << endl;
                         exit(0);
                     }
 
-                    if(buf2 == "<person>") {//если закрыли тег личности, то обнуляем переменную
-                        choice = 0;
+                    if(buf2 == tags[1]) {//если закрыли тег личности, то обнуляем переменную
                         check(Answer, search, write);
+                        write.clear();
                     }
                 }
-
-                if(buf2 == "<name>") //если открыли тег имени - меняем переменную на 1
-                    choice = 1;
-                if(buf2 == "<ID>") //если тег ID - 2
-                    choice = 2;
             }
 
             else {//Если у нас нет тега
+                user buffer;
                 pos = buf.find("<"); //ищем открытие тега
                 string buf2(buf, 0, pos);// создаем строку с тегом
                 string buf_for_next(buf, pos, buf.size()); // создаем строку с остатком
                 buf = buf_for_next; //присваиваем изначальной строке остаток
-                if(choice == 1) //если в переменной 1, то записываем имя
-                    write.name = buf2;
-                if(choice == 2) //если 2, то ID
-                    write.id = buf2;
+                buffer.tag = tags[tags.size() - 1];
+                buffer.request = buf2;
+                write.push_back(buffer);
             }
         }
 
@@ -96,19 +99,15 @@ void parser_xml (int &Answer, string &search) {
             break;
     }
 
-    for(int i = 0; i < tags.size(); i++) {
-        cout << endl << tags[i];
-    }
-
     base.close(); //закрываем файл
 }
 
-void Menu (int &Answer, string &find) {
+void Menu (int &Answer, user &find) {
     cout << "Choose what you want to find: ";
-    cout << endl << "1 - by the name";
-    cout << endl << "2 - by ID";
-    cout << endl << "3 - show all";
-    cout << endl << "4 - exit";
+    cout << endl << "1 - by tag";
+    cout << endl << "2 - show all";
+    cout << endl << "3 - exit";
+    cout << endl << "4 - write all data to file";
     for(;;) {//чтобы не вводили недействительные числа
         cout << endl;
         cin >> Answer;
@@ -118,26 +117,35 @@ void Menu (int &Answer, string &find) {
     cin.ignore(); // игнорируем значение в потоке ввода, чтобы вводить строку
     system("cls");
     if (Answer == 1) {
-        cout << "Enter the name: ";
-        getline(cin, find);
-    }
-    if (Answer == 2) {
-        cout << "Enter ID: ";
-        getline(cin, find);
+        cout << "Enter the request: ";
+        getline(cin, find.request);
+        cout << endl << "Enter the tag, like <name>: ";
+        getline(cin, find.tag);
     }
 }
 
 int main() {
     int Answer;
-    string found; //Имя или ID
+    user found; //Имя или ID
+
+    ifstream base ("base.xml");
+    if(!base.is_open()) {//если xml файл отсутствует
+        cout << endl << "ERROR! base.xml wasn't created. Put this file near exe file of this program!" << endl; //выводим ошибку
+        exit(0); //выходим
+    }
+    base.close();
 
     for(;;) {
         system("cls"); // очистка экрана
         Menu(Answer, found); // меню выбора имени или ID
         system("cls");
-        if (Answer == 4)
+        if (Answer == 3)
             exit(0);
         else {
+            if(Answer == 4) {
+                ofstream file("data.txt");//открываем файл, чтобы data.txt был пустым
+                file.close();
+            }
             parser_xml(Answer, found);
             system("pause");
         }
